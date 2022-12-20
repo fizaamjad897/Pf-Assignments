315d140a2bdd4bc71decce6c8999b190793a2232
fizaamjad897 <120313148+fizaamjad897@users.noreply.github.com>
2022-12-20 21:49:33 +0500

    Add files via upload

---
+++ 20centurytext.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<< "    **********            eeeee        eeeeee            ***********    " <<endl;
+cout<< "     *********           e     e      e      e           **********     " <<endl;
+cout<< "      ********              eeee      e      e           *********      " <<endl;
+cout<< "      ********             ee         e      e           ********       " <<endl;
+cout<< "       *******            eeeeeee      eeeeee th         *******        " <<endl;
+cout<< "        ******          .........................        ******         " <<endl;
+cout<< "         *****            C  E  N  T  U  R  Y            *****          " <<endl;  
+cout<< "          ****          .........................        ****           " <<endl;
+cout<< "           ***          eeeee eeeee  e   e  eeeee        ***            " <<endl;
+cout<< "            **            e   e       e e     e          **             " <<endl;
+cout<< "          __||__          e   eeeee    e      e        __||__           " <<endl;
+cout<< "         |      |         e   e       e e     e       |      |          " <<endl;
+cout<< "  _______|______|_______  e   eeeee  e   e    e ______|______|_________ " <<endl;
+}
